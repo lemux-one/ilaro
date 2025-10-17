b99851bb719674c6e130764b2719b34fa6a7d605
Luis Lemus <lemux.one@gmail.com>
2025-10-17 14:42:16 -0400

    [refactor] added internal HTML namespace to better organize JSX type definitions

--- jsx/jsx.namespace.ts
+++ jsx/jsx.namespace.ts
@@ -1,8 +1,14 @@
 export declare namespace JSX {
   type LiteralElement = string | number;
 
-  interface WithChildren {
-    children: LiteralElement | LiteralElement[] | JSX.Element | JSX.Element[];
+  interface WithChildren extends CommonAttrs {
+    children:
+      | LiteralElement
+      | LiteralElement[]
+      | JSX.Element
+      | JSX.Element[]
+      | null
+      | undefined;
   }
 
   type ComponentProps = CommonAttrs & Record<string, unknown>;
@@ -18,21 +24,41 @@ export declare namespace JSX {
     props: Partial<ComponentProps & WithChildren> & Record<string, unknown>;
   }
 
-  interface CommonAttrs {
-    class?: string;
-    id?: string;
-    style?: string;
-  }
+  type CommonAttrs = Partial<{
+    class: string;
+    id: string;
+    style: string;
+  }>;
+
+  interface ContainerElementAttrs extends Partial<WithChildren> {}
 
   interface IntrinsicElements {
-    html: WithChildren & CommonAttrs & { lang?: string };
-    div: WithChildren & CommonAttrs;
-    span: WithChildren & CommonAttrs;
+    html: HTML.Html;
+    div: HTML.Div;
+    span: HTML.Span;
+    a: HTML.Anchor;
+    img: HTML.Img;
     [key: string]: CommonAttrs & any;
   }
 
   type FC<
-    T extends keyof IntrinsicElements,
+    T extends keyof IntrinsicElements = "div",
     P extends ComponentProps = IntrinsicElements[T],
   > = (props: P & IntrinsicElements[T]) => JSX.Element;
+}
+
+declare namespace HTML {
+  type Html = JSX.ContainerElementAttrs & {
+    lang?: string;
+  };
+  type Div = JSX.ContainerElementAttrs;
+  type Span = JSX.ContainerElementAttrs;
+  type Anchor = JSX.ContainerElementAttrs & {
+    href: string;
+    target?: "_blank" | "_self" | "_parent" | "_top";
+  };
+  type Img = JSX.ContainerElementAttrs & {
+    src: string;
+    alt: string;
+  };
 }
