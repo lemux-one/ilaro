a51ecc850ce76a187cbca13d4c304adcecc283d7
Luis Lemus <lemux.one@gmail.com>
2025-10-17 14:01:12 -0400

    [refactor] export all from namespace and utils

--- jsx/index.ts
+++ jsx/index.ts
@@ -1,23 +1,2 @@
-export declare namespace JSX {
-  interface WithChildren {
-    children?: JSX.Element | JSX.Element[] | string | string[];
-  }
-
-  interface Element {
-    type: string | Function;
-    props: WithChildren & Record<string, unknown>;
-  }
-
-  interface CommonAttrs {
-    class?: string;
-    id?: string;
-    style?: string;
-  }
-
-  interface IntrinsicElements {
-    html: WithChildren & CommonAttrs & { lang?: string };
-    div: WithChildren & CommonAttrs;
-    span: WithChildren & CommonAttrs;
-    [key: string]: CommonAttrs & any;
-  }
-}
+export * from "./jsx.namespace";
+export * from "./utils";
